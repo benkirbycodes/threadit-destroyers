2be376d30e150877747ba9314d0900a04c527788
Ben Kirby <54716741+benkirbycodes@users.noreply.github.com>
2019-12-13 12:11:01 -0700

    Resolves review

--- server/controllers/CommentsController.js
+++ server/controllers/CommentsController.js
@@ -6,21 +6,12 @@ export default class CommentsController {
     this.router = express
       .Router()
       //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
-      // .get("", this.getAll)
       .get("/:id", this.getById)
       .post("", this.create)
       .put("/:id", this.edit)
       .delete("/:id", this.delete);
   }
 
-  // async getAll(req, res, next) {
-  //   try {
-  //     let data = await commentsService.getAll();
-  //     return res.send(data);
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // }
   async getById(req, res, next) {
     try {
       let data = await commentsService.getById(req.params.id);
