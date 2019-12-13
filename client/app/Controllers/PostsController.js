a0af852a144a0b828dfa3fdd4bf0c30fabe5482a
reedm26 <reedm26@yahoo.com>
2019-12-13 13:48:59 -0700

    Add Post Form Appears

--- client/app/Controllers/PostsController.js
+++ client/app/Controllers/PostsController.js
@@ -2,15 +2,43 @@ import PostsService from "../Services/PostsService.js";
 import store from "../store.js";
 
 //Private
-function _draw() {
+function _drawPosts() {
   let posts = store.State.Posts;
   console.log(posts);
+}
+function drawForm() {
+  let template = `<form onsubmit="app.PostsController.addPostAsync(event)">
+  <div class="form-group">
+    <label for="title">Title</label>
+    <input type="text" class="form-control" id="title" aria-describedby="title" placeholder="Enter Title...">
+  </div>
+  <div class="form-group">
+    <label for="body">Opening Line</label>
+    <input type="text" class="form-control" id="body" placeholder="Start Your Story...">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>`;
+  document.querySelector("#posts").innerHTML = template;
 }
 
 //Public
 export default class PostsController {
   constructor() {
-    store.subscribe("posts", _draw);
+    store.subscribe("posts", _drawPosts);
+  }
+  drawForm() {
+    let template = `<form onsubmit="app.PostsController.addPostAsync(event)">
+    <div class="form-group">
+      <label for="title">Title</label>
+      <input type="text" class="form-control" id="title" aria-describedby="title" placeholder="Enter Title...">
+    </div>
+    <div class="form-group">
+      <label for="body">Opening Line</label>
+      <input type="text" class="form-control" id="body" placeholder="Start Your Story...">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>`;
+    document.querySelector("#posts").innerHTML = template;
   }
   async addPostAsync(event) {
     event.preventDefault();
--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -6,7 +6,18 @@ export default class Post {
     this.userId = data.userId;
   }
 
-  get Template() {
-    return this.title;
+  get addPostTemplate() {
+    return `
+    <form onsubmit="app.PostsController.addPostAsync(event)">
+  <div class="form-group">
+    <label for="title">Title</label>
+    <input type="text" class="form-control" id="title" aria-describedby="title" placeholder="Enter Title...">
+  </div>
+  <div class="form-group">
+    <label for="body">Opening Line</label>
+    <input type="text" class="form-control" id="body" placeholder="Start Your Story...">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>`;
   }
 }
