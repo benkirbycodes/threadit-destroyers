cf9ab5db0535f6ef5d2e30027beded708682f109
Gerax Sotelo <gsotelo23@gmail.com>
2019-12-13 10:21:10 -0700

    Create user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,12 @@
 
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
 
 const User = new Schema(
   {
     name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    image: { type: String, default: "https://robohash.org/user" }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
